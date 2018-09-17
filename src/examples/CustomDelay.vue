91d956ef71c6f4fc0bfbbf18e52073e00165778b
Gaiaz Iusipov <g.iusipov@gmail.com>
2018-09-18 02:47:04 +0300

    Divide examples

---
+++ src/examples/CustomDelay.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <image-slider
+      v-model="slides"
+      v-bind:delay="delay"
+    />
+  </div>
+</template>
+
+<script>
+import ImageSlider from '@/App'
+
+export default {
+  data () {
+    return {
+      slides: [
+        'https://dummyimage.com/300&text=1',
+        'https://dummyimage.com/300&text=2',
+        'https://dummyimage.com/300&text=3'
+      ],
+      delay: 2000
+    }
+  },
+  components: {
+    ImageSlider
+  }
+}
+</script>
---
+++ src/examples/Indicators.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <image-slider v-model="slides">
+      <div slot="indicator" slot-scope="{ indicator }" />
+    </image-slider>
+  </div>
+</template>
+
+<script>
+import ImageSlider from '@/App'
+
+export default {
+  data () {
+    return {
+      slides: [
+        'https://placeimg.com/300/300/animals',
+        'https://placeimg.com/300/300/arch',
+        'https://placeimg.com/300/300/nature',
+        'https://placeimg.com/300/300/people'
+      ]
+    }
+  },
+  components: {
+    ImageSlider
+  }
+}
+</script>
+
+<style>
+.image-slider__indicator {
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  border: 2px solid white;
+}
+.image-slider__indicator_active {
+  background-color: white;
+}
+.image-slider__indicator_loading {
+  background-color: white;
+  animation: fadeIn 0.3s infinite alternate;
+}
+@keyframes fadeIn {
+  from { opacity: 0; }
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .3s
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0
+}
+</style>
